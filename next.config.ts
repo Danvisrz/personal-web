b3d6203e92bb7915b04682b9ec365c00d2d885fe
Danvisrz <arazqidanvi@gmail.com>
2026-08-29 08:39:01 +0800

    fix: add basepath for images on github

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   images: {
-    unoptimized: true, // Wajib untuk GitHub Pages
+    unoptimized: true, 
   },
+  basePath: "/personal-web", // <-- TAMBAHKAN BARIS INI
 };
 
 export default nextConfig;
